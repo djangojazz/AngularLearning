ddf0bed5f074697ce005270f3ae467444d45f580
Brett Morin <brettmorin2@gmail.com>
2018-04-04 15:58:58 -0700

    Updated to use reactive forms

--- Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
+++ Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from "@angular/forms";
 import { Customer } from './customer';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit  {
+    customerForm: FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: true
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: 'Brett',
+            lastName: 'Test',
+            sendCatalog: false
+        });
     }
  }
