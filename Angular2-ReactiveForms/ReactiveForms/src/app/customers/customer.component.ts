4dd2164c9267994893ff3c471000cc4f5179069d
Brett Morin <brettmorin2@gmail.com>
2018-04-05 13:28:57 -0700

    Added validators

--- Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
+++ Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { Customer } from './customer';
 
 @Component({
@@ -14,16 +14,13 @@ export class CustomerComponent implements OnInit  {
 
     ngOnInit(): void {
         this.customerForm = this.fb.group({
-            firstName: '',
-            lastName: '',
-            email: '',
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            email: ['', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+")]],
+            phone: '',
+            notification: 'email',
             sendCatalog: true
         });
-    }
-
-    save() {
-        console.log(this.customerForm);
-        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
 
     populateTestData(): void {
@@ -32,5 +29,20 @@ export class CustomerComponent implements OnInit  {
             lastName: 'Test',
             sendCatalog: false
         });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if(notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
  }
