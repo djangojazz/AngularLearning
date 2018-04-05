806cdc1315541aaf7bc13406ce77e7ea593ef10f
Brett Morin <brettmorin2@gmail.com>
2018-04-05 14:03:43 -0700

    Added contained validator

--- Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
+++ Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
@@ -25,7 +25,10 @@ export class CustomerComponent implements OnInit  {
         this.customerForm = this.fb.group({
             firstName: ['', [Validators.required, Validators.minLength(3)]],
             lastName: ['', [Validators.required, Validators.maxLength(50)]],
-            email: ['', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+")]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+")]],
+                confirmEmail: ['', Validators.required],
+            }),
             phone: '',
             notification: 'email',
             rating: ['', ratingRange(1,5)],
