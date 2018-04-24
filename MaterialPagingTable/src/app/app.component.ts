195069c43f0033969a89f4b110661a0d1ff4a5bf
djangojazz <brettmorin2@gmail.com>
2018-04-24 07:13:21 -0700

    Got data working

--- MaterialPagingTable/src/app/app.component.ts
+++ MaterialPagingTable/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { TestContainer } from './TestContainer';
 
 import {Observable} from 'rxjs/Observable';
 import {merge} from 'rxjs/observable/merge';
@@ -16,46 +17,48 @@ import { TestService  } from "./test.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'app';
   displayedColumns = ['id', 'description'];
-  dataSource = new MatTableDataSource();
-
-  resultsLength = 0;
-  isLoadingResults = true;
-  isRateLimitReached = false;
+  dataSource: MatTableDataSource<TestContainer>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private testService: TestService) {}
-
-  ngOnInit() {
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    this.testService.loadSlice(5, 0)
-      .subscribe(() => this.dataSource.data = this.testService.tests);
-
-    merge(this.sort.sortChange, this.paginator.page, this.paginator.pageSize)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.isLoadingResults = true;
-          return this.testService.loadSlice(this.paginator.pageSize, this.paginator.pageIndex);
-        }),
-        map(data => {
-          // Flip flag to show that loading has finished.
-          this.isLoadingResults = false;
-          this.isRateLimitReached = false;
-          this.resultsLength = this.testService.tests.length;
-
-          return data;
-        }),
-        catchError(() => {
-          this.isLoadingResults = false;
-          // Catch if the GitHub API has reached its rate limit. Return empty data.
-          this.isRateLimitReached = true;
-          return observableOf([]);
-        })
-      ).subscribe(data => this.dataSource.data = data);
+  constructor(private testService: TestService) {
+    this.dataSource = new MatTableDataSource(this.testService.tests);
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  // ngOnInit() {
+  //   this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+  //   this.tests = this.testService.tests;
+  //   this.dataSource.data = this.tests;
+
+  //   merge(this.sort.sortChange, this.paginator.page, this.paginator.pageSize)
+  //     .pipe(
+  //       startWith({}),
+  //       switchMap(() => {
+  //         this.isLoadingResults = true;
+  //         return this.testService.loadSlice(this.paginator.pageSize, this.paginator.pageIndex);
+  //       }),
+  //       map(data => {
+  //         // Flip flag to show that loading has finished.
+  //         this.isLoadingResults = false;
+  //         this.isRateLimitReached = false;
+  //         this.resultsLength = this.testService.tests.length;
+
+  //         return data;
+  //       }),
+  //       catchError(() => {
+  //         this.isLoadingResults = false;
+  //         // Catch if the GitHub API has reached its rate limit. Return empty data.
+  //         this.isRateLimitReached = true;
+  //         return observableOf([]);
+  //       })
+  //     ).subscribe(data => this.dataSource.data = data);
+  // }
 }
