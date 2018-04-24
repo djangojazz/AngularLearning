4a3977d9f39da07a5127674b83b5cf31014214d6
Brett Morin <brettmorin2@gmail.com>
2018-04-24 12:59:32 -0700

    Added an ability to iterate over form array

--- Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
+++ Angular2-ReactiveForms/ReactiveForms/src/app/customers/customer.component.ts
@@ -71,6 +71,10 @@ export class CustomerComponent implements OnInit  {
             this.setMessage(emailControl));
     }
 
+    addAddress(): void {
+        this.addresses.push(this.buildAddress());
+    }
+
     buildAddress(): FormGroup {
         return this.fb.group({
             addressType: 'home',
