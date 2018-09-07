f9c6af804befcaac2af44e92d5cb0dc8f060c62d
Brett Morin <brettmorin2@gmail.com>
2018-09-07 13:10:20 -0700

    Finished lazy loading module

--- Angular2-Routing/src/app/app-routing.module.ts
+++ Angular2-Routing/src/app/app-routing.module.ts
@@ -4,17 +4,24 @@ import { RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
-      { path: 'products', canLoad: [AuthGuard], loadChildren: 'app/products/product.module#ProductModule' },
+      { 
+        path: 'products', 
+        canActivate: [AuthGuard], 
+        data: { preload: true },
+        loadChildren: 'app/products/product.module#ProductModule' 
+      },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent }
     ]
-    , { enableTracing: true })
+    , { enableTracing: true, preloadingStrategy: SelectiveStrategy })
   ],
+  providers: [ SelectiveStrategy],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
